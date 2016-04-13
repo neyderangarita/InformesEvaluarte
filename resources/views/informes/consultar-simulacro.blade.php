8c239fbb317024fa2ce34d6218e6077eb4d5ba5d
neyder angarita osorio <neyder6101@hotmail.com>
2016-04-13 13:59:48 -0500

    realiza ajuste vista simulacros

--- resources/views/informes/consultar-simulacro.blade.php
+++ resources/views/informes/consultar-simulacro.blade.php
@@ -11,9 +11,23 @@
 			<?= $lava->render('ColumnChart', 'Simulacros', 'perf_div') ?>
 		</div>
 
-	    <div class="col-sm-6" style="text-align: center">
-			<img src="/imagenes/individual.jpg"  width="20%" height="20%" />
-	    </div>
+		@if($informes[0]->simulacro == 'Tu saber')		
+		<!--<div class="col-sm-6" style="text-align: center">
+				<img src="/imagenes/prueba-tu-saber.png"  width="70%" height="70%" />
+		    </div> -->					
+			<div class="col-sm-6">
+				<div class="thumbnail">
+					<img src="/imagenes/prueba-tu-saber.png"  width="auto" height="auto" />
+				</div>
+			</div>
+
+	    @else					 
+			<div class="col-sm-6">
+				<div class="thumbnail">
+					<img src="/imagenes/otras-pruebas.jpg"  width="50%" height="auto" />
+				</div>
+			</div>
+	    @endif
 
 	</div>
 </div>
@@ -26,19 +40,21 @@
 					<div class="caption">	
 
 					    <H3>Simulacro {{ $index+1 }}</H3>
-						<!-- <p><strong>Tipo Simulacros:</strong> {{ $informe->simulacro }}</p> -->
-						<p><strong>Total:</strong> {{ $informe->proTotal }}</p>
-						<p><strong>Fecha Aplico:</strong> {{ $informe->FechaAplico }}</p>
- 						
- 						@if($informe->simulacro == 'Tu saber')
+ 						@if($informe->simulacro == 'Tu saber')					
+ 						<p><strong>Fecha Aplico:</strong> {{ $informe->FechaAplico }}</p>
 	 						@if($informe->grado == 'JARDÍN' || $informe->grado == 'PREJARDÍN' || $informe->grado == 'TRANSICIÓN')
+	 							<p><strong>Total:</strong> {{ ($informe->proMat1 + $informe->proMat2 + $informe->proMat3) / 3}}</p>
 								<p><a href="/validado/informes/generar-saber-basica/{{$informe->id}}" target="_blank" class="btn btn-primary" role="button">Ver Simulacro</a></p>					
 							@elseif($informe->grado == '10°' || $informe->grado == '11°')						    
+								<p><strong>Total:</strong> {{ ($informe->proMat1 + $informe->proMat2 + $informe->proMat3 + $informe->proMat4 + $informe->proMat5) / 5}}</p>
 								<p><a href="/validado/informes/generar-saber-superior/{{$informe->id}}" target="_blank" class="btn btn-primary" role="button">Ver Simulacro</a></p>						
-							@else	
+							@else
+								<p><strong>Total:</strong> {{ ($informe->proMat1 + $informe->proMat2 + $informe->proMat3 + $informe->proMat4 + $informe->proMat5) / 5}}</p>	
 								<p><a href="/validado/informes/generar-saber-media/{{$informe->id}}" target="_blank" class="btn btn-primary" role="button">Ver Simulacro</a></p>						
 							@endif
 						@elseif($informe->simulacro == 'saber 10 y 11 4 Preguntas Abiertas' || $informe->simulacro == 'saber 4,6,7 y 8' || $informe->simulacro == 'saber 3_Nuevo' || $informe->simulacro == 'saber 5_Nuevo')					 
+						<p><strong>Total:</strong> {{ $informe->proTotal }}</p>
+						<p><strong>Fecha Aplico:</strong> {{ $informe->FechaAplico }}</p>
 							@if($informe->grado == '10°' || $informe->grado == '11°')
 								<p><a href="/validado/informes/generar-media-vocacional/{{$informe->codigo}}/{{$informe->codigo_simulacro}}" target="_blank" class="btn btn-primary" role="button">Ver Simulacro</a></p>					
 							@elseif($informe->grado == '3°' || $informe->grado == '4°')						    
