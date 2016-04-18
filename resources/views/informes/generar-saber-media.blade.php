00353d1be0cc467fd5b7e7bd4d1069ac36dd8e72
neyder angarita osorio <neyder6101@hotmail.com>
2016-04-18 07:31:11 -0500

    menor

--- resources/views/informes/generar-saber-media.blade.php
+++ resources/views/informes/generar-saber-media.blade.php
@@ -8,35 +8,31 @@
         <div style="text-align: center; font: 80%; font-family: Geneva, Cambria, monospace, Arial, Helvetica, sans-serif">
                 <img class="custom_logo ie_png"  style="position: absolute;top: 0px;left: 0px;width: auto;" src="/imagenes/fondo-saber-media.png">
                 
-                <div style="position: absolute;top: 318px;left: 130px;width: 400px;"  id="perf_div">
+                <div style="position: absolute;top: 294px;left: 91px;width: 400px;"  id="perf_div">
                     <?= $lava->render('ColumnChart', 'Simulacros', 'perf_div') ?>
                 </div>
 
-                <div style="position: absolute;top: 761px;left: 225px;width: 50px;">
+                <div style="position: absolute;top: 731px;left: 185px;width: 50px;">
                     <span>{{ $informar->proMat1 }}<span>&nbsp;</span>
                 </div>
-
-                <div style="position: absolute;top: 761px;left: 310px;width: 86px;">
+                <div style="position: absolute;top: 731px;left: 270px;width: 86px;">
                     <span>{{ $informar->proMat2}}<span>&nbsp;</span>
                 </div>
-
-                <div style="position: absolute;top: 761px;left: 410px;width: 86px;">
+                <div style="position: absolute;top: 731px;left: 370px;width: 86px;">
                     <span>{{ $informar->proMat3}}<span>&nbsp;</span>
                 </div>
-
-                <div style="position: absolute;top: 761px;left: 512px;width: 86px;">
+                <div style="position: absolute;top: 731px;left: 472px;width: 86px;">
                     <span>{{ $informar->proMat4}}<span>&nbsp;</span>
                 </div>
-
-                <div style="position: absolute;top: 761px;left: 613px;width: 86px;">
+                <div style="position: absolute;top: 731px;left: 573px;width: 86px;">
                     <span>{{ $informar->proMat5}}<span>&nbsp;</span>
                 </div>
 
-                <div style="position: absolute;top: 830px;left: 340px;width: 86px;">
+                <div style="position: absolute;top: 798px;left: 300px;width: 86px;">
                     <span>{{ round(($informar->proMat1 + $informar->proMat2 + $informar->proMat3 + $informar->proMat4 + $informar->proMat5)/ 5)}}<span>&nbsp;</span>
                 </div>
-
-                <div style="position: absolute;top: 830px;left: 600px;width: 86px;">
+                
+                <div style="position: absolute;top: 798px;left: 560px;width: 86px;">
                     <span>
                         <?php 
                             $media = round(($informar->proMat1 + $informar->proMat2 + $informar->proMat3 + $informar->proMat4 + $informar->proMat5)/ 5);
@@ -54,33 +50,31 @@
                     <span>&nbsp;</span>
                 </div>
             <!-- Datos del personales del estudiante -->
-            <div style="position: absolute;top: 188px;left: 126px;width: 350px;">
+            <div style="position: absolute;top: 157px;left: 86px;width: 350px;">
                 <span>{{ $informar->NombreEstudiante }}<span>&nbsp;</span>
             </div>
-
-            <div style="position: absolute;top: 188px;left: 486px;width: 250px;">
+            <div style="position: absolute;top: 157px;left: 446px;width: 250px;">
                 <span>{{ $informar->colegio }}<span>&nbsp;</span>
             </div>
-
-            <div style="position: absolute;top: 225px;left: 586px;width:116px;">
+           <div style="position: absolute;top: 198px;left: 556px;width:116px;">
                 <span>{{ $informar->codigo }}<span>&nbsp;</span>
             </div>
-
-            <div style="position: absolute;top: 225px;left: 387px;width:116px;">
+            <div style="position: absolute;top: 198px;left: 357px;width:116px;">
                 <span>{{ $informar->FechaAplico }}<span>&nbsp;</span>
             </div>
 
-            <div style="position: absolute;top: 225px;left: 218px;width:116px;">
+            <div style="position: absolute;top: 198px;left: 190px;width:116px;">
                 <span>{{ $informar->puesto }}<span>&nbsp;</span>
             </div>
-
-            <div style="position: absolute;top: 225px;left: 84px;width: 116px;">
+            <div style="position: absolute;top: 198px;left: 60px;width: 116px;">
                 <span>{{ $informar->grado }}<span>&nbsp;</span>
             </div>
-            <!-- Datos PUNTAJE materias  -->
-        <div style="position: absolute;left: 0px;width: 0px;">
-             <button  type="button" class="btn btn-default" onClick="window.print()">Imprimir</button>
-        </div>
+            <div style="position: absolute;left: 0px;width: 0px;">                             
+                <a  onClick="window.print();return false" href="#"> 
+                    <img width="auto" height="45px" src="/imagenes/impresora.png">  
+                </a>
+            </div>
+
         </div>
     </body>
 </html>
