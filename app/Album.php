06d1f960947424a334384858585f788170ffcb4a
neyderosorio <neyder.angarita@processpluserp.com>
2018-04-26 10:23:43 -0500

    Elimina album , foto , Informe

--- app/Album.php
+++ app/Album.php
@@ -4,16 +4,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Album extends Model
 {
-	protected $table = 'albumes';
-	protected $fillable = ['id', 'nombre', 'descripcion', 'usuario_id'];
 
-	public function fotos()
-	{
-		return $this->hasMany('GestorImagenes\Foto');
-	}
-
-	public function propietario()
-	{
-		return $this->belongsTo('GestorImagenes\Usuario');	
-	}
 }
--- app/Foto.php
+++ app/Foto.php
@@ -4,11 +4,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Foto extends Model
 {
-	protected $table = 'fotos';
-	protected $fillable = ['id', 'nombre', 'descripcion', 'ruta', 'album_id'];
 
-	public function album()
-	{
-		return $this->belongsTo('GestorImagenes\Album');
-	}
 }
